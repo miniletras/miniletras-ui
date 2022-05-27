4bc2205cf3bd1df15482a94e933ef2ab3df18a89
Fernando Rguez <frmonteira@gmail.com>
2022-05-27 06:58:28 +0200

    feat(App): minor types changes

--- components.d.ts
+++ components.d.ts
@@ -2,31 +2,31 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    Article: typeof import('./src/components/Articles/Article.vue')['default']
-    CarbonBackToTop: typeof import('~icons/carbon/back-to-top')['default']
-    CarbonCalendar: typeof import('~icons/carbon/calendar')['default']
-    CarbonHome: typeof import('~icons/carbon/home')['default']
-    CarbonMenu: typeof import('~icons/carbon/menu')['default']
-    CarbonMoon: typeof import('~icons/carbon/moon')['default']
-    CarbonSearch: typeof import('~icons/carbon/search')['default']
-    CarbonSun: typeof import('~icons/carbon/sun')['default']
-    CarbonTableOfContents: typeof import('~icons/carbon/table-of-contents')['default']
-    CarbonWarningAltFilled: typeof import('~icons/carbon/warning-alt-filled')['default']
-    Footer: typeof import('./src/components/Footer/Footer.vue')['default']
-    ImageThree: typeof import('./src/components/Image/ImageThree.vue')['default']
-    ImageTwo: typeof import('./src/components/Image/ImageTwo.vue')['default']
-    Latest: typeof import('./src/components/Articles/Latest.vue')['default']
-    Navbar: typeof import('./src/components/Navigation/Navbar.vue')['default']
-    Paginate: typeof import('./src/components/Pagination/Paginate.vue')['default']
-    Post: typeof import('./src/components/Articles/Post.vue')['default']
-    Share: typeof import('./src/components/Button/Share.vue')['default']
-    Tag: typeof import('./src/components/Button/Tag.vue')['default']
-    ToTopBtn: typeof import('./src/components/Button/ToTopBtn.vue')['default']
-    UilDocumentLayoutCenter: typeof import('~icons/uil/document-layout-center')['default']
-    UilGithub: typeof import('~icons/uil/github')['default']
+    Article: typeof import("C:/test/elucidator-blog-starter/src/components/Articles/Article.vue")["default"]
+    CarbonBackToTop: typeof import("~icons/carbon/back-to-top")["default"]
+    CarbonCalendar: typeof import("~icons/carbon/calendar")["default"]
+    CarbonHome: typeof import("~icons/carbon/home")["default"]
+    CarbonMenu: typeof import("~icons/carbon/menu")["default"]
+    CarbonMoon: typeof import("~icons/carbon/moon")["default"]
+    CarbonSearch: typeof import("~icons/carbon/search")["default"]
+    CarbonSun: typeof import("~icons/carbon/sun")["default"]
+    CarbonTableOfContents: typeof import("~icons/carbon/table-of-contents")["default"]
+    CarbonWarningAltFilled: typeof import("~icons/carbon/warning-alt-filled")["default"]
+    Footer: typeof import("./src/components/Footer/Footer.vue")["default"]
+    ImageThree: typeof import("./src/components/Image/ImageThree.vue")["default"]
+    ImageTwo: typeof import("./src/components/Image/ImageTwo.vue")["default"]
+    Latest: typeof import("./src/components/Articles/Latest.vue")["default"]
+    Navbar: typeof import("./src/components/Navigation/Navbar.vue")["default"]
+    Paginate: typeof import("./src/components/Pagination/Paginate.vue")["default"]
+    Post: typeof import("./src/components/Articles/Post.vue")["default"]
+    Share: typeof import("./src/components/Button/Share.vue")["default"]
+    Tag: typeof import("./src/components/Button/Tag.vue")["default"]
+    ToTopBtn: typeof import("./src/components/Button/ToTopBtn.vue")["default"]
+    UilDocumentLayoutCenter: typeof import("~icons/uil/document-layout-center")["default"]
+    UilGithub: typeof import("~icons/uil/github")["default"]
   }
 }
 
-export { }
+export {}
