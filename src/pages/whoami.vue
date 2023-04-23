b4b369f196524c15692f9b0520603a396d09e746
Fernando Rguez <frmonteira@gmail.com>
2023-04-23 18:38:05 +0200

    feat(whoami): complete circle styles

--- src/pages/whoami.vue
+++ src/pages/whoami.vue
@@ -8,6 +8,7 @@ const title = ref(" whoami.vue Component!")
     <div class="main-section border-red">
       <section class="main-section__figure">
         <figure class="main-section__personal-photo"></figure>
+        <div class="main-section__square-1"></div>
       </section>
     </div>
     <div class="col-1 border-red"></div>
@@ -25,15 +26,23 @@ const title = ref(" whoami.vue Component!")
   justify-content: center;
   &__figure {
     border-radius: 100%;
-    background-color: $color-gray-1;
-    border: 1rem solid $color-gray-1;
   }
   &__personal-photo {
-    background: no-repeat center/80% url("../images/cata_de_mar_whoami.jpeg");
-    @include heightWidth(16rem, 16rem);
+    background: no-repeat center 66%/97% url("../images/cata_de_mar_whoami-circle.png");
+    @include heightWidth(28rem, 28rem);
     border-radius: 100%;
-    background-color: $color-gray-1;
-    border-radius: 4px solid $color-purple-1;
+    background-color: white;
+    border: 1.75rem solid $color-pink-1;
+    z-index: 1;
+    overflow: visible;
+    position: relative;
+  }
+  &__square-1 {
+    @include heightWidth(10rem, 28rem);
+    background-color: $color-pink-1;
+    margin-top: -14rem;
+    z-index: 0;
+    position: relative;
   }
 }
 </style>
